b933b6e1555510201dac29b689d70b5bb5c75fe0
Akash Pattnaik <akashpattnaik@Akashs-MacBook-Pro.local>
2023-02-16 20:44:53 -0500

    first sub

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -13,6 +13,7 @@ class CONFIG:
     initial_weight_decay = 0
 
     lrs_kwargs = {
+        "lr_lambda": lambda epoch: 0.95**epoch,
         # You can pass arguments to the learning rate scheduler
         # constructor here.
     }
@@ -20,7 +21,9 @@ class CONFIG:
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,21 +4,45 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    A custom learning rate scheduler. This is a wrapper around the PyTorch _LRScheduler class.
+    """
+
+    def __init__(self, optimizer, lr_lambda, last_epoch=-1):
         """
         Create a new scheduler.
 
         Note to students: You can change the arguments to this constructor,
         if you need to add new parameters.
 
+        Adapted from: https://pytorch.org/docs/stable/_modules/torch/optim/lr_scheduler.html
+
         """
-        # ... Your Code Here ...
+        self.optimizer = optimizer
+        self.last_epoch = last_epoch
+
+        if not isinstance(lr_lambda, list) and not isinstance(lr_lambda, tuple):
+            self.lr_lambdas = [lr_lambda] * len(optimizer.param_groups)
+        else:
+            if len(lr_lambda) != len(optimizer.param_groups):
+                raise ValueError(
+                    f"Expected {len(optimizer.param_groups)} lr_lambdas, but got {len(lr_lambda)}"
+                )
+            self.lr_lambdas = list(lr_lambda)
+
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
+        """
+        Return the current learning rate. This is called internally by Torch.
+        """
+
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
-        # ... Your Code Here ...
+        return [
+            base_lr * lmbda(self.last_epoch)
+            for lmbda, base_lr in zip(self.lr_lambdas, self.base_lrs)
+        ]
         # Here's our dumb baseline implementation:
         return [i for i in self.base_lrs]
