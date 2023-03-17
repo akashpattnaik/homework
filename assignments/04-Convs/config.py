d17696331afa5f8a786c0c32baf27b1682e49c9f
Akash Pattnaik <akashpattnaik@SEASNet-14-44.seas.upenn.edu>
2023-03-17 15:52:42 -0400

    new update

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,12 +6,14 @@ from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 32
-    num_epochs = 2
+    batch_size = 40
+    num_epochs = 1
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
+    ] = lambda model: torch.optim.Adam(
+        model.parameters(), lr=5.5e-3, weight_decay=0.001
+    )
 
     transforms = Compose(
         [ToTensor(), Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))]
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -17,15 +17,29 @@ class Model(torch.nn.Module):
 
         super(Model, self).__init__()
 
-        self.conv1 = nn.Conv2d(num_channels, 6, 5)
+        self.conv1 = nn.Conv2d(
+            num_channels,
+            num_channels,
+            kernel_size=5,
+            padding=1,
+            dilation=1,
+            groups=num_channels,
+        )
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
-        # initialize our softmax classifier
-        # self.fc2 = torch.nn.Linear(in_features=500, out_features=num_classes)
-        # self.logSoftmax = torch.nn.LogSoftmax(dim=1)
+        self.conv2 = nn.Conv2d(
+            num_channels,
+            num_channels * 6,
+            kernel_size=5,
+            padding=1,
+            dilation=1,
+            groups=num_channels,
+        )
+        self.fc1 = nn.Linear(num_channels * 6 * 6 * 6, 184)
+        nn.init.kaiming_normal_(self.fc1.weight)
+        # self.fc2 = nn.Linear(88, 55)
+        # nn.init.kaiming_normal_(self.fc2.weight)
+        self.fc3 = nn.Linear(184, num_classes)
+        nn.init.kaiming_normal_(self.fc3.weight)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass of the model.
@@ -35,12 +49,11 @@ class Model(torch.nn.Module):
             output (torch.Tensor): The output of the model.
         """
 
-        # pass the input through our first set of CONV => RELU =>
-        # POOL layers
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        output = self.fc3(x)
+        # x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
         return output
